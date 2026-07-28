dee14abdf95d539ba085d2e645d5c8df0ae1b8c9
Amir Suhail <amirsuhail@Pops.local>
2026-07-29 00:28:27 +0530

    clean delivery partner client

--- server/routes/owner/restaurant.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getRestaurant, updateRestaurant } from '../../controllers/owner/restaurant.controller.js';
-
-const router = Router({ mergeParams: true });
-
-router.get('/', getRestaurant);
-router.patch('/', updateRestaurant);
-
-export default router;
